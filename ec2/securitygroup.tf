8ab4d6f92ad7d35c3981d5f639f88ce910e92449
mlnawsdevops <mlnpublicact365@gmail.com>
2026-01-05 21:01:31 +0530

    terraform commands

--- ec2/securitygroup.tf
+++ ec2/securitygroup.tf
@@ -17,5 +17,9 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks = [ "0.0.0.0/0" ]
         ipv6_cidr_blocks = [ "::/0" ]
     }
+
+    tags = {
+        Name = "allow-ssh-port"
+    }
   
 }
